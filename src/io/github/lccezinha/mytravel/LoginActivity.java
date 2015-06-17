678f53909707b3cf9e404f81017475aa8d76a675
Luiz Cezer <lccezinha@gmail.com>
2015-06-16 21:56:36 -0300

    Adicionando botão entrar e finalizando xml da view de login

---
+++ src/io/github/lccezinha/mytravel/LoginActivity.java
@@ -0,0 +1,18 @@
+package io.github.lccezinha.mytravel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+	
+	public void login(View v) {
+	}
+	
+}
