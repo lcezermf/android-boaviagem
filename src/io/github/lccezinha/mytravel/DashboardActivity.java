a057bd004b793cf05547dfced335ec7faf4de183
Luiz Cezer <lccezinha@gmail.com>
2015-06-18 21:37:59 -0300

    Ajuste de login + tela básica de dashboard

--- src/io/github/lccezinha/mytravel/DashboardActivity.java
+++ src/io/github/lccezinha/mytravel/DashboardActivity.java
@@ -1,7 +1,23 @@
 package io.github.lccezinha.mytravel;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashboardActivity extends Activity {
-
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+	}
+	
+	public void selectOption(View view){
+		TextView textView = (TextView) view;
+		String option = "Opção: " + textView.getText().toString();
+		Toast.makeText(this, option, Toast.LENGTH_LONG).show();
+	}
+	
 }
