27283c5ff37569dcda433f9b74733b11ea75ab57
Luiz Cezer <lccezinha@gmail.com>
2015-07-13 21:37:00 -0300

    WIP: List views

--- src/io/github/lccezinha/mytravel/SpentListActivity.java
+++ src/io/github/lccezinha/mytravel/SpentListActivity.java
@@ -1,42 +1,78 @@
 package io.github.lccezinha.mytravel;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SpentListActivity extends ListActivity implements
 		OnItemClickListener {
 
+	private List<Map<String, Object>> spents;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
-		setListAdapter(
-				new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listSpents())
-		);
-		ListView listView = getListView();
-		listView.setOnItemClickListener(this);
+		String[] from = { "date", "description", "value", "category" };
+		int[] to = { R.id.date, R.id.description, R.id.value, R.id.category };
+		
+		SimpleAdapter adapter = new SimpleAdapter(this, listSpents(), R.layout.list_spent, from, to);
+		
+		setListAdapter(adapter);
+		getListView().setOnItemClickListener(this);
 	}
 	
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 		
-		TextView textView = (TextView) view;
-		Toast.makeText(this,"Gasto selecionado: " + textView.getText(), Toast.LENGTH_SHORT).show();
+		Map<String, Object> map = spents.get(position);
+		String description = (String) map.get("description");
+		String message = "Gasto Selecionado: " + description;
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 	}
 	
-	private List<String> listSpents(){
-		return Arrays.asList("Dogão: R$ 20.00", "Taxi - Volta: R$ 35.00", "Coisas: R$ 50.00");
+	private List<Map<String, Object>> listSpents(){
+		spents = new ArrayList<Map<String, Object>>();
+		
+		Map<String, Object> item = new HashMap<String, Object>();
+		item.put("date", "03/03/2013");
+		item.put("description", "Hotel");
+		item.put("value", "R$ 41.00");
+		item.put("category", R.color.categoria_hospedagem);
+		spents.add(item);
+		
+		item = new HashMap<String, Object>();
+		item.put("date", "03/03/2013");
+		item.put("description", "Lanche");
+		item.put("value", "R$ 30.00");
+		item.put("category", R.color.categoria_alimentacao);
+		spents.add(item);
+		
+		return spents;
+	}
+	
+	private class SpentViewBinder implements ViewBinder {
+		
+		public boolean setViewValue(View view, Object data, String textRepresentation) {
+			return false;
+		}
+		
 	}
 
 }
--- src/io/github/lccezinha/mytravel/TravelListActivity.java
+++ src/io/github/lccezinha/mytravel/TravelListActivity.java
@@ -1,7 +1,10 @@
 package io.github.lccezinha.mytravel;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -11,36 +14,59 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TravelListActivity extends ListActivity implements
 		OnItemClickListener {
 	
+	private List<Map<String, Object>> travels;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
-		setListAdapter(
-				new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listTravels())
-		);
-		ListView listView = getListView();
-		listView.setOnItemClickListener(this);
+		String[] from = { "image", "destino", "data", "total" };
+		int[] to = { R.id.travelKind, R.id.destiny, R.id.date, R.id.value };
+		
+		SimpleAdapter adapter = new SimpleAdapter(this, listTravels(), R.layout.list_travel, from, to);
+		
+		setListAdapter(adapter);
+		
+		getListView().setOnItemClickListener(this);
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 		
-		TextView textView = (TextView) view;
-		String message = "Viagem selecionada: " + textView.getText().toString();
-		
-		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+		Map<String, Object> map = travels.get(position);
+		String destiny = (String) map.get("destiny");
+		String message = "Viagem Selecionada: " + destiny;
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 		startActivity(new Intent(this, SpentListActivity.class));
+		
 	}
 	
-	private List<String> listTravels(){
-		return Arrays.asList("São Paulo", "Joinville", "Floarinópolis");
+	private List<Map<String, Object>> listTravels(){
+		travels = new ArrayList<Map<String, Object>>();
+		
+		Map<String, Object> item = new HashMap<String, Object>();
+		item.put("image", R.drawable.negocios);
+		item.put("destiny", "São Paulo");
+		item.put("data", "02/02/2012 a 04/02/2012");
+		item.put("total", "Gasto total de R$: 313.21");
+		travels.add(item);
+		
+		item = new HashMap<String, Object>();
+		item.put("image", R.drawable.lazer);
+		item.put("destiny", "Joinville");
+		item.put("data", "03/03/2013 a 07/03/2013");
+		item.put("total", "Gasto total de R$: 555.12");
+		travels.add(item);
+		
+		return travels;
 	}
 
 }
