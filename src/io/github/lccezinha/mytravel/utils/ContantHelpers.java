e21af0de1a057e6c6b7534ed919542076690cc93
Luiz Cezer <lccezinha@gmail.com>
2015-07-24 16:41:00 -0300

    Método: Adicionar viagem :punch:

---
+++ src/io/github/lccezinha/mytravel/utils/ContantHelpers.java
@@ -0,0 +1,10 @@
+package io.github.lccezinha.mytravel.utils;
+
+public class ContantHelpers {
+	public static final String TRAVEL_MODE = "travel_mode";
+	public static final String TRAVEL_ID = "travel_id";
+	public static final String TRAVEL_DESTINY = "travel_destiny";
+	public static final String SELECT_TRAVEL_MODE = "select_travel_mode";
+	public static final int LEISURE_TRAVEL = 1;
+	public static final int BUSINESS_TRAVEL = 2;
+}
