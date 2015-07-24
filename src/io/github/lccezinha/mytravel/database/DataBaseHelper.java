dcec9ea939b87b42af81b1e275c1d01a74babba4
Luiz Cezer <lccezinha@gmail.com>
2015-07-24 13:35:59 -0300

    criação da base de dados

--- src/io/github/lccezinha/mytravel/database/DataBaseHelper.java
+++ src/io/github/lccezinha/mytravel/database/DataBaseHelper.java
@@ -7,21 +7,33 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-	public DataBaseHelper(Context context, String name, CursorFactory factory, int version) {
-		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
+	private static final String DATABASE = "boaviagem";
+	private static int VERSION = 1;
+	
+	public DataBaseHelper(Context context) {
+		super(context, DATABASE, null, VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		createTableTravels(db);
+		createTableSpents(db);		
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+	
+	private void createTableTravels(SQLiteDatabase db){
+		db.execSQL("CREATE TABLE TRAVELS (_ID INTEGER PRIMARY KEY," +
+				" DESTINY TEXT, TRAVEL_KIND INTEGER, DATE_FINISH DATE," +
+				" DATE_START DATE, BUDGET DOUBLE, PEOPLE_QUANTITY INTEGER);");
 	}
-
+	
+	private void createTableSpents(SQLiteDatabase db){
+		db.execSQL("CREATE TABLE SPENTS (_ID INTEGER PRIMARY KEY," +
+				" CATEGORGY TEXT, SPENT_DATE DATE, VALUE DOUBLE, DESCRIPTION TEXT," +
+				" PLACE TEXT, TRAVEL_ID INTEGER," +
+				" FOREIGN KEY(TRAVEL_ID) REFERENCES TRAVELS(_ID));");		
+	}
+	
 }
